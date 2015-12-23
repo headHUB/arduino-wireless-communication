8d3d166e058f64309177f505559e8e09d7e66f5b
deloarts <deloarts@outlook.com>
2015-12-23 16:40:58 +0100

    Updated readme and added serial monitor

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Wireless_Communication
-Arduino: Sending data via the NRF24L01+ module.
+# Wireless Communication
+This small script will show you how to use the NRF24L01+ module to send data wireless from one Arduino to another.
+
+### Description
+
+Here you will find 2 sketches. The first one, the transmitter sketch, will constantly send the values from the analog pins A0, A1 and A2 via the module to the other one.
+Therefor the receiver sketch will catch the data and print it to the small display.
+If you don't have such a display, don't worry, just delete the // from the **#define USE_SERIAL** line.
+After this, you can use the serial monitor from the Arduino IDE to see incomming data.
+
+###Hardware
+
+**Required components:**
+- Arduino UNO or Nano (2 pcs)
+- NRF24L01+ module (2 pcs)
+- 0.96" I2C Display module (1 pcs)
+- Jumper wires
+
+**Connection for the NRF24L01+:**
+
+Arduino Pin | NRF24L01+ Pin
+------------|--------------
+9			| CE
+10			| CSN
+11			| MOSI
+12			| MISO
+13			| SCK
+3V3			| VCC
+GND			| GND
+
+**Connection for the display:**
+
+Arduino Pin | Display Pin
+------------|------------
+A4			| SDA
+A5			| SCL
+5V			| VCC
+GND			| GND
+
+
+
+For further instructions, please visit my blog:
--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -5,12 +5,15 @@
 	This is the receiver.
 	It constantly waits for a message from the transmitter.
 */
+//#define USE_SERIAL
 // Get neccessary header files for NRF24
 #include <SPI.h>
 #include "RF24.h"
 // and for the little display
+#ifndef USE_SERIAL
 #include <OLED_I2C_128x64_Monochrome.h>
 #include <Wire.h>
+#endif
 
 // Define the radio addresses (must be the same on both devices!)
 byte addresses[][6] = {"DevRX", "DevTX"};
@@ -28,9 +31,16 @@ struct dataStruct
 // ##################################################################################################################### 
 void setup()
 {
+	#ifndef USE_SERIAL
 	// Initialize the display
 	lcd.initialize();
 	lcd.rotateDisplay180(); // I need to rotate the screen, maybe you don't need this line ...
+	#endif
+
+	#ifdef USE_SERIAL
+	Serial.begin(9600);
+	#endif
+
 	// Initialize the NRF24
 	radio.begin();
 	// Set the PA level to high, so the message can be sent over long distances.
@@ -46,11 +56,18 @@ void setup()
 	radio.openWritingPipe(addresses[1]);
 	radio.openReadingPipe(1, addresses[0]);
 
-	// Print a message on the display
+	#ifndef USE_SERIAL
+	// Print a message on the display ...
 	lcd.clearDisplay();
 	lcd.printString("Sensor 1: ", 0, 2);
 	lcd.printString("Sensor 2: ", 0, 4);
 	lcd.printString("Sensor 3: ", 0, 6);
+	#endif
+
+	#ifdef USE_SERIAL
+	// ... or to the serial port
+	Serial.print("Ready for receiving data.");
+	#endif
 
 	radio.startListening();
 }
@@ -63,13 +80,24 @@ void loop()
 	{
 		// Get the data from the NRF24
 		radio.read(&Sensor, sizeof(Sensor));
-		// and print it to the display
+		
+		#ifndef USE_SERIAL
+		// and print it to the display ...
 		lcd.printString("    ", 11, 2);
 		lcd.printNumber(long(Sensor.Sensor1), 11, 2);
 		lcd.printString("    ", 11, 4);
 		lcd.printNumber(long(Sensor.Sensor2), 11, 4);
 		lcd.printString("    ", 11, 6);
 		lcd.printNumber(long(Sensor.Sensor3), 11, 6);
+		#endif
+
+		#ifdef USE_SERIAL
+		// ... or to the serial port
+		Serial.print("Sensor 1: "); Serial.println(Sensor.Sensor1);
+		Serial.print("Sensor 2: "); Serial.println(Sensor.Sensor2);
+		Serial.print("Sensor 3: "); Serial.println(Sensor.Sensor3);
+		Serial.print("\n");
+		#endif
 	}
 }
 // ##################################################################################################################### 
